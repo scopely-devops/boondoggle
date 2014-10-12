eaa02f477999d6166f4cc49cab0907191014a6ce
Anthony Grimes <i@raynes.me>
2014-10-12 09:22:30 -0700

    Clean up lengthy lines and inconsistent prints.

--- boondoggle/managers.py
+++ boondoggle/managers.py
@@ -14,15 +14,14 @@
 
 import time
 
-import boto.cloudformation
-from boto.cloudformation.stack import Stack
+import boto.cloudformation as cf
 from boto.exception import BotoServerError
 
 
 class DeployManager(object):
     def __init__(self, region, profile):
         self.region = region
-        self.cloudformation = boto.cloudformation.connect_to_region(self.region, profile_name=profile)
+        self.cf = cf.connect_to_region(self.region, profile_name=profile)
 
     def ensure(self, name, url, parameters):
         # First we check if the stack exists
@@ -30,17 +29,22 @@ class DeployManager(object):
 
         try:
             if status is None:
-                self.cloudformation.create_stack(stack_name=name, template_url=url, parameters=parameters)
+                self.cf.create_stack(stack_name=name,
+                                     template_url=url,
+                                     parameters=parameters)
             else:
-                self.cloudformation.update_stack(stack_name=name, template_url=url, parameters=parameters)
+                self.cf.update_stack(stack_name=name,
+                                     template_url=url,
+                                     parameters=parameters)
         except BotoServerError, ex:
             if ex.status == 403:
-                print("Forbidden! Provided credentials cannot create the stack")
-                print ex.body
+                print("Forbidden! Provided credentials "
+                      "cannot create the stack")
+                print(ex.body)
                 exit(1)
             elif ex.status == 400:
                 print("Could not create stack")
-                print ex.body
+                print(ex.body)
                 exit(1)
             else:
                 raise
@@ -48,12 +52,12 @@ class DeployManager(object):
         self.wait_for_completion(name)
 
     def cancel_update(self, name):
-        self.cloudformation.cancel_update_stack(stack_name_or_id=name)
+        self.cf.cancel_update_stack(stack_name_or_id=name)
         self.wait_for_completion(name)
 
     def status(self, name):
         try:
-            events = self.cloudformation.describe_stack_events(stack_name_or_id=name)
+            events = self.cf.describe_stack_events(stack_name_or_id=name)
             if events is not None:
                 return events[0]
             else:
@@ -62,20 +66,23 @@ class DeployManager(object):
             if ex.status == 400:
                 return None
             if ex.status == 403:
-                print("Forbidden! Cannot check status with provided credentials")
+                print("Forbidden! Cannot check status "
+                      "with provided credentials")
                 exit(1)
             else:
                 raise
 
     def wait_for_completion(self, name):
-        good_completed_status = ['CREATE_COMPLETE', 'UPDATE_COMPLETE', 'DELETE_COMPLETE']
+        good_completed_status = ['CREATE_COMPLETE',
+                                 'UPDATE_COMPLETE',
+                                 'DELETE_COMPLETE']
 
         bad_status = ['ROLLBACK_IN_PROGRESS', 'UPDATE_ROLLBACK_IN_PROGRESS',
                       'UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS']
 
-        bad_completed_status = ['CREATE_FAILED', 'ROLLBACK_FAILED', 'ROLLBACK_COMPLETE',
-                                'DELETE_FAILED', 'DELETE_COMPLETE',
-                                'UPDATE_ROLLBACK_FAILED',
+        bad_completed_status = ['CREATE_FAILED', 'ROLLBACK_FAILED',
+                                'ROLLBACK_COMPLETE', 'DELETE_FAILED',
+                                'DELETE_COMPLETE', 'UPDATE_ROLLBACK_FAILED',
                                 'UPDATE_ROLLBACK_COMPLETE']
 
         # Give 45 minutes for operation completion
@@ -90,7 +97,8 @@ class DeployManager(object):
             assert time.time() < timeout
             status_info = self.status(name)
 
-            if last_resource == status_info.physical_resource_id and last_status == status_info.resource_status:
+            if (last_resource == status_info.physical_resource_id
+                    and last_status == status_info.resource_status):
                 time.sleep(1)
                 elapsed += 1
                 continue
@@ -110,21 +118,25 @@ class DeployManager(object):
 
             status = status_info.resource_status
             if status in bad_status:
-                print("({2}s) Stack {0} in intermediate failure status {1}, waiting for a final status"
+                print("({2}s) Stack {0} in intermediate failure "
+                      "status {1}, waiting for a final status"
                       .format(name, status, elapsed))
                 continue
 
             if status in bad_completed_status:
-                print("({2}s) Stack {0} in status {1}; failed to apply template"
+                print("({2}s) Stack {0} in status {1}; "
+                      "failed to apply template"
                       .format(name, status, elapsed))
                 return False
 
             if status in good_completed_status:
-                print("({2}s) Stack {0} in final status {1}; succeeded in applying template"
+                print("({2}s) Stack {0} in final status {1}; "
+                      "succeeded in applying template"
                       .format(name, status, elapsed))
                 return True
 
-            print("({2}s) Stack {0} in intermediate status {1}, waiting for a final status"
+            print("({2}s) Stack {0} in intermediate status {1}, "
+                  "waiting for a final status"
                   .format(name, status, elapsed))
             time.sleep(interval)
             elapsed += interval
